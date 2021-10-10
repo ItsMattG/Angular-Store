4f1885ebd018f888907bdd815ce97235acf99c56
Matt Gleeson <gleesonmatt96@gmail.com>
2021-10-10 17:30:05 +1100

    Added product, product-list components & store module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+import { ShopReducer } from './store/reducer';
+import { ShopEffects } from './store/effects';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductComponent,
+    ProductListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    StoreModule.forRoot({ shop: ShopReducer }),
+    EffectsModule.forRoot([ShopEffects]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Product } from '../product/product.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {
+  constructor(private store: Store<{ items: []; cart: [] }>) {
+    store.pipe(select('shop')).subscribe((data) => (this.cart = data.cart));
   }
 
-  cart = [];
-  ngOnInit() { }
+  cart: Product[] = [];
+  ngOnInit() {}
 }
